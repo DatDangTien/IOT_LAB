98fbc89eed86055c72a692429a256d5ffd397d79
DatDangTien <116250191+DatDangTien@users.noreply.github.com>
2024-03-29 10:09:56 +0700

    face_detect

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from Adafruit_IO import MQTTClient
 import time, random
-
+from detector import face_detector
 
 AIO_FEED_ID = ["button1", "button2"]
 AIO_USERNAME = "Dat_iot"
@@ -30,19 +30,26 @@ client.on_subscribe = subscribe
 client.connect()
 client.loop_background()
 
-counter = 10
+previous_detect = None
+counter = 20
 while True:
     counter -= 1
     if counter <= 0:
         # Reset counter
-        counter = 10
+        counter = 20
         # Update to server
-        print("Publish to server ...")
+        print("Publish sensor to server ...")
         temp = random.randint(10,100)
         client.publish(feed_id="sensor1", value=temp)
         light = random.randint(10,1000)
         client.publish(feed_id="sensor2", value=light)
         humid = random.randint(10,100)
         client.publish(feed_id="sensor3", value=humid)
+    if counter % 3 == 0:
+        detect = face_detector()
+        if previous_detect != face_detector():
+            client.publish(feed_id="model", value=detect)
+            print("Publish detect to server ...")
+            previous_detect = detect
 
     time.sleep(1)
